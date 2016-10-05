44431bc4bece4cfa5515eb0f59c550a137ac0f5a
Tobias Kunze <rixx@cutebit.de>
2016-10-05 23:37:06 +0200

    Print a nicely formatted table

--- bepasty-client/client.py
+++ bepasty-client/client.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+from datetime import datetime
+import json
+
 import click
 
 from helpers import handle_request
@@ -37,6 +40,18 @@ def list(password, url, insecure):
     click.echo('Date'.ljust(20) + '│ ', nl=False)
     click.echo('URL')
     click.echo('─' * 100)
+
+    for name, paste in json.loads(response.content).items():
+        meta = paste['file-meta']
+        name = meta['filename']
+        name = name[:18] + '…' if len(name) > 19 else name
+        created = datetime.fromtimestamp(meta['timestamp-upload']).isoformat()
+
+        click.echo(name.ljust(20) + '│ ', nl=False)
+        click.echo(meta['type'][:19].ljust(19) + '│ ', nl=False)
+        click.echo(str(meta['size'])[:9].rjust(9) + ' │ ', nl=False)
+        click.echo(created.ljust(20) + '│ ', nl=False)
+        click.echo(f'{url}/{name}')
 
 
 if __name__ == '__main__':
